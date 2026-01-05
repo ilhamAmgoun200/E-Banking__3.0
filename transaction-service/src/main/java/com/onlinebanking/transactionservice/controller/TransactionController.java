e1070813477734148b00114a4a9e204345070b3e
Ahlam-amz <a.amziane9670@uca.ac.ma>
2026-01-05 23:04:32 +0100

    notif problem solved

--- transaction-service/src/main/java/com/onlinebanking/transactionservice/controller/TransactionController.java
+++ transaction-service/src/main/java/com/onlinebanking/transactionservice/controller/TransactionController.java
@@ -4,57 +4,7 @@ import com.onlinebanking.transactionservice.model.Transaction;
 import com.onlinebanking.transactionservice.service.TransactionService;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-/*
-@RestController
-@RequestMapping("/transactions")
-public class TransactionController {
-    private final TransactionService transactionService;
 
-    public TransactionController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }
-
-    @GetMapping
-    public List<Transaction> getAllTransactions() {
-        return transactionService.getAllTransactions();
-    }
-
-    @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        return transactionService.createTransaction(transaction);
-    }
-
-    @GetMapping("/user/{username}")
-    public List<Transaction> getTransactionsByUsername(@PathVariable String username) {
-        return transactionService.getTransactionsByUsername(username);
-    }
-
-    @GetMapping("/account/{accountNumber}")
-    public List<Transaction> getTransactionsByAccountNumber(@PathVariable String accountNumber) {
-        return transactionService.getTransactionsByAccountNumber(accountNumber);
-    }
-
-    @GetMapping("/account/{accountNumber}/all")
-    public List<Transaction> getAllTransactionsForAccount(@PathVariable String accountNumber) {
-        return transactionService.getAllTransactionsForAccount(accountNumber);
-    }
-
-    @PostMapping("/transfer")
-    public List<Transaction> createTransferTransactions(@RequestBody java.util.Map<String, Object> request) {
-        String fromAccountNumber = (String) request.get("fromAccountNumber");
-        String toAccountNumber = (String) request.get("toAccountNumber");
-        Double amount = (Double) request.get("amount");
-        String fromUsername = (String) request.get("fromUsername");
-        String toUsername = (String) request.get("toUsername");
-        String description = (String) request.get("description");
-
-        return transactionService.createTransferTransactions(fromAccountNumber, toAccountNumber,
-                amount, fromUsername, toUsername, description);
-    }
-}*/
-
-
-import com.onlinebanking.transactionservice.model.Transaction;
 import com.onlinebanking.transactionservice.service.AuditProducerService;
 import com.onlinebanking.transactionservice.service.TransactionService;
 import com.onlinebanking.transactionservice.dto.AuditEventType;
@@ -63,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 @RestController
@@ -174,9 +124,17 @@ public class TransactionController {
     @PostMapping("/transfer")
     public List<Transaction> createTransferTransactions(@RequestBody java.util.Map<String, Object> requestBody,
                                                         HttpServletRequest request) {
+
+        Double amount;
+        Object amountObj = requestBody.get("amount");
+        if (amountObj instanceof Integer) {
+            amount = ((Integer) amountObj).doubleValue();
+        } else {
+            amount = (Double) amountObj;
+        }
         String fromAccountNumber = (String) requestBody.get("fromAccountNumber");
         String toAccountNumber = (String) requestBody.get("toAccountNumber");
-        Double amount = (Double) requestBody.get("amount");
+        //Double amount = (Double) requestBody.get("amount");
         String fromUsername = (String) requestBody.get("fromUsername");
         String toUsername = (String) requestBody.get("toUsername");
         String description = (String) requestBody.get("description");
